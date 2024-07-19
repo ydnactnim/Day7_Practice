eeed41defceba7182528ac3dff43979b2a06cc14
ydnactnim <ydnactnim@gmail.com>
2024-07-19 16:57:01 +0900

    Luck
    Luck

---
+++ lucky/main.js
@@ -0,0 +1,7 @@
+const startButton = document.querySelector("button-box");
+const init = document.querySelector("init-out");
+const luck = document.querySelector("luck-out");
+
+function selectLuck() {
+    const index = math.floor(Math.random() * WebTransportDatagramDuplexStream.length[])
+}
